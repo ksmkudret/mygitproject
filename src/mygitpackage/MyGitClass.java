feb3782281a77e3e7e52e6318fb82373564e3b3a
ksmkudret <81583673+ksmkudret@users.noreply.github.com>
2021-04-06 22:12:27 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -7,6 +7,7 @@ public class MyGitClass {
 		System.out.println("My second commit");
 		System.out.println("My third commit");
 		System.out.println("My code at remote repository");//
+		System.out.println("My 5th code ");
 	}
 
 }
